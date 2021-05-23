a81d315be70a80c77c0a403f69f0e5cfe54aa86f
ShahinaEP <shahinaeasminpia860703@gmail.com>
2021-05-22 17:46:30 -0800

    3rd upload

--- MY_FIRST_NODE_WEBSITE/index.js
+++ MY_FIRST_NODE_WEBSITE/index.js
@@ -2,13 +2,28 @@ var fs = require('fs');
 var http = require('http');
 
 var server = http.createServer(function(req,res){
-    if(req.url=="/"){
-        fs.readFile('Home.html',function(error,data){
-           res.writeHead(200,{'Content-Type':'text/html'});
-           res.write(data);
-           res.end(); 
-        });
-    }
+    // if(req.url=="/"){
+    //     //Asynchronous
+    //     fs.readFile('Home.html',function(error,data){
+    //        res.writeHead(200,{'Content-Type':'text/html'});
+    //        res.write(data);
+    //        res.end(); 
+    //     });
+    // }
+    
+     //Synchronous
+     if(req.url =="/"){
+        let data = fs.readFileSync('Home.html','utf8');
+        res.end(data)
+     }
+     else if(req.url =="/About"){
+         let data = fs.readFileSync('About.html','utf8');
+         res.end(data);
+     }
+     else if(req.url=="/Contact"){
+         let data = fs.readFileSync('Contact.html','utf8');
+         res.end(data);
+     }
 });
 
 server.listen(4040);
